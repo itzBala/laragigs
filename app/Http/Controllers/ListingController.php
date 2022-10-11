a8198b25aa1754b4b91db87d62a52cda863a88ae
Your Name <you@example.com>
2022-10-11 14:38:01 +0530

    Tags Filter & Search Function Added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -12,7 +12,7 @@ class ListingController extends Controller
     {
         return view('pages.listings.index', [
             'heading' => 'Latest Listings',
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
